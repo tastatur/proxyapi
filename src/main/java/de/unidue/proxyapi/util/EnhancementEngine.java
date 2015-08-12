235878616587bfcfa1e0aa01effb2848fb7bef5f
tastatur <repkov.denis@gmail.com>
2015-08-12 10:16:50 +0200

    Type fixed

--- src/main/java/de/unidue/proxyapi/util/EnhancementEngine.java
+++ src/main/java/de/unidue/proxyapi/util/EnhancementEngine.java
@@ -1,7 +1,7 @@
 package de.unidue.proxyapi.util;
 
 public enum EnhancementEngine {
-    STANFORD("stanford-nerchain"), TIGER("tiger-nerhcain"), PIG("pig-nerchain");
+    STANFORD("stanford-nerchain"), TIGER("tiger-nerchain"), PIG("pig-nerchain");
 
     private final String engineName;
 
