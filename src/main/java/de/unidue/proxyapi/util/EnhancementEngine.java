58ce772536f74d89ba70e7b1dff3a69a00ceea2f
tastatur <repkov.denis@gmail.com>
2015-08-19 20:00:54 +0200

    MITIE Engine

--- src/main/java/de/unidue/proxyapi/util/EnhancementEngine.java
+++ src/main/java/de/unidue/proxyapi/util/EnhancementEngine.java
@@ -2,6 +2,7 @@ package de.unidue.proxyapi.util;
 
 public enum EnhancementEngine {
     STANFORD_BOTH("stanford-nerchain-both"), STANFORD_DEWAC("stanford-nerchain-dewac"), STANFORD_HGC("stanford-nerchain-hgc"),
+    MITIE_PIG("mitie-nerchain-pig"), MITIE_TIGER("mitie-nerchain-tiger"),
     TIGER("tiger-nerchain"), PIG("pig-nerchain");
 
     private final String engineName;
